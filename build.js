6c0b2bf3e736989ff1556e14e778ea7d6c974024
jkmnt <git@firewood.fastmail.com>
2024-06-27 01:50:15 +0300

    * sync package.json with the actual grammar triggers

--- build.js
+++ build.js
@@ -3,5 +3,11 @@ const fs = require("fs");
 const gen_grammar = require("./out/gen_grammar");
 
 const result = gen_grammar.gen(gen_grammar.DEF_SPECS);
-const json = JSON.stringify(result, null, 4);
-fs.writeFileSync("./syntaxes/embed-in-python.json", json)
+fs.writeFileSync("./syntaxes/embed-in-python.json", JSON.stringify(result, null, 4));
+
+// patch package.json defaults for the lang triggers
+defs = Object.fromEntries(gen_grammar.DEF_SPECS.map((spec) => [spec.lang, spec.trigger]));
+package_json = JSON.parse(fs.readFileSync("package.json"));
+package_json.contributes.configuration.properties["python-fstring-dsl.grammar.triggers"].default =
+  defs;
+fs.writeFileSync("package.json", JSON.stringify(package_json, null, 4));
